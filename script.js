b57161b29fa39f73276e11f4823b371846898d74
leesovska <leesovska@gmail.com>
2024-05-02 21:08:13 +0300

    Домашнее задание № 11
    Тренажер слепой печати. При загрузке страницы в div.word подставляется случайное слово. Пользователь вводит его по буквам.
    Если текущий символ введен правильно, он окрашивается в зеленый. Если неправильно, то в красный. В случае неправильного ввода текущий символ требуется повторно ввести (любое количество раз — до победного).
    В конце все слово становится зеленым.

---
+++ script.js
@@ -0,0 +1,46 @@
+let words = ['apple', 'banana', 'cherry', 'apricot', 'avocado', 'pineapple',
+'grapefruit', 'kiwi', 'lime', 'lemon', 'mango', 'melon', 'orange', 'papaya',
+'peach', 'pear', 'persimmon', 'plum', 'pomegranate', 'pomelo', 'tangerine', 'quince'];
+
+let currentWord = '';
+let currentIndex = 0;
+
+window.onload = function() {
+  currentWord = getRandomWord();
+  displayWord(currentWord);
+}
+
+
+function getRandomWord() {
+  let index = Math.floor(Math.random() * words.length);
+  return words[index];
+}
+
+function displayWord(word) {
+  const wordDiv = document.querySelector('.word');
+  wordDiv.innerHTML = '';
+  for(let i = 0; i < word.length; i++) {
+    const span = document.createElement('span');
+    span.textContent = word[i];
+    wordDiv.append(span);
+  }
+}
+
+document.addEventListener('keypress', function(event) {
+  const letter = event.key;
+  let span = document.querySelector('.word').children[currentIndex];
+  if(letter === currentWord[currentIndex]) {
+    span.classList.add('c');
+    currentIndex++;
+    if(currentIndex === currentWord.length) {
+      document.querySelector('.correct-count').textContent++;
+      currentWord = getRandomWord();
+      displayWord(currentWord);
+      currentIndex = 0;
+    }
+  } else {
+    span.classList.add('w');
+    document.querySelector('.wrong-count').textContent++;
+    document.querySelector('.word-mistakes').textContent++;
+  }
+});
